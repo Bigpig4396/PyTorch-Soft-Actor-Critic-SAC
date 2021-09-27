d2d5c9ecb07b843f9c9fbc23168ec72846917469
Shuo Jiang <jiangshuo456@126.com>
2021-09-27 16:48:10 -0400

    Update SAC.py

--- SAC.py
+++ SAC.py
@@ -35,8 +35,8 @@ class P_net(nn.Module):
         self.fc3 = nn.Linear(128, action_dim)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = torch.sigmoid(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
         action_score = self.fc3(x)
         return F.softmax(action_score, dim=-1)
 
@@ -47,27 +47,11 @@ class Q_net(nn.Module):
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, action_dim)
 
-        self.fc4 = nn.Linear(state_dim, 256)
-        self.fc5 = nn.Linear(256, 128)
-        self.fc6 = nn.Linear(128, action_dim)
-
     def forward(self, x):
-        h = F.relu(self.fc1(x))
-        h = F.relu(self.fc2(h))
-        q1 = self.fc3(h)
-
-        h = F.relu(self.fc1(x))
-        h = F.relu(self.fc2(h))
-        q2 = self.fc3(h)
-        return q1, q2
-
-def soft_update(target, source, tau):
-    for target_param, param in zip(target.parameters(), source.parameters()):
-        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
-
-def hard_update(target, source):
-    for target_param, param in zip(target.parameters(), source.parameters()):
-        target_param.data.copy_(param.data)
+        h = torch.sigmoid(self.fc1(x))
+        h = torch.sigmoid(self.fc2(h))
+        q = self.fc3(h)
+        return q
 
 class SAC():
     def __init__(self, state_dim, action_dim):
@@ -76,25 +60,17 @@ class SAC():
         self.action_dim = action_dim
         self.p_net = P_net(state_dim, action_dim)
         self.q_net = Q_net(state_dim, action_dim)
-        self.q_target_net = Q_net(state_dim, action_dim)
         self.gamma = 0.99
-        self.log_alpha = torch.zeros(1, dtype=torch.float32, requires_grad=True)
-        self.alpha = self.log_alpha.exp()
+        self.alpha = 1
         self.loss_fn = torch.nn.MSELoss()
         self.q_optimizer = torch.optim.Adam(self.q_net.parameters(), lr=1e-3)
         self.p_optimizer = torch.optim.Adam(self.p_net.parameters(), lr=1e-4)
-        self.alpha_optimizer = optim.Adam([self.log_alpha], lr=1e-5)
-        hard_update(self.q_target_net, self.q_net)
-        self.update_interval = 100
-        self.update_count = 0
 
     def get_action(self, state):
         state = torch.from_numpy(state).float()
         action_prob = self.p_net.forward(state)
         c = Categorical(action_prob)
         action = c.sample()
-        # print('q', q.detach().numpy())
-        # print('a_prob', action_prob.detach().numpy())
         return action.item()
 
     def train(self, batch):
@@ -107,28 +83,23 @@ class SAC():
         T = state.size()[0]
 
         # calculate V
-        self.alpha = self.log_alpha.exp()
-        next_q1, next_q2 = self.q_target_net.forward(next_state)
-        next_q = torch.min(next_q1, next_q2)
+        next_q = self.q_net.forward(next_state)
         next_a_prob = self.p_net.forward(next_state)
         next_v = next_a_prob*(next_q-self.alpha*torch.log(next_a_prob))
         next_v = torch.sum(next_v, 1)
 
         # train Q
-        q1, q2 = self.q_net.forward(state)
-        expect_q = q1.clone()
+        q = self.q_net.forward(state)
+        expect_q = q.clone()
         for i in range(T):
             expect_q[i, action[i]] = reward[i] + self.gamma * next_v[i]
-        loss1 = self.loss_fn(q1, expect_q.detach())
-        loss2 = self.loss_fn(q2, expect_q.detach())
-        loss = loss1 + loss2
+        loss = self.loss_fn(q, expect_q.detach())
         self.q_optimizer.zero_grad()
         loss.backward()
         self.q_optimizer.step()
 
         # train Actor
-        q1, q2 = self.q_net.forward(state)
-        q = torch.min(q1, q2)
+        q = self.q_net.forward(state)
         a_prob = self.p_net.forward(state)
         ploss = a_prob*(self.alpha*torch.log(a_prob)-q)
         ploss = torch.sum(ploss)
@@ -136,21 +107,6 @@ class SAC():
         self.p_optimizer.zero_grad()
         ploss.backward()
         self.p_optimizer.step()
-
-        # Updating alpha wrt entropy
-        self.alpha = self.log_alpha.exp()
-        a_prob = self.p_net.forward(state)
-        H = - a_prob*torch.log(a_prob)
-        alpha_loss = -a_prob*(self.alpha*torch.log(a_prob) + self.alpha * H)
-        alpha_loss = torch.sum(alpha_loss)
-        alpha_loss = alpha_loss / T
-        self.alpha_optimizer.zero_grad()
-        alpha_loss.backward()
-        self.alpha_optimizer.step()
-        # print('q_loss', loss.detach().item(), 'p_loss', ploss.detach().item(), 'alpha_loss', alpha_loss.detach().item())
-        self.update_count += 1
-        if self.update_count % self.update_interval == 0:
-            soft_update(self.q_target_net, self.q_net, 0.7)
 
     def load_model(self):
         self.q_net = torch.load('SAC_q_net.pkl')
